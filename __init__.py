fc7a3bf6b082156cbe24308241c0a99f8f60a563
Sergey Malinovskiy <malinovskiy.sergey.olegovich@gmail.com>
2026-05-01 21:45:13 +0300

    ignore stegano

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .src.cylmore_steganography_kit import *
--- src/cylmore_steganography_kit/masking/lsb_stegano_masking.py
+++ src/cylmore_steganography_kit/masking/lsb_stegano_masking.py
@@ -1,11 +1,11 @@
-from stegano import lsb
+# from stegano import lsb
 
 from cylmore_steganography_kit.src.cylmore_steganography_kit.encrypt import IFileMaskingNode, ICanSaving
 
 
 class LsbSteganoMasking(IFileMaskingNode):
     def hide(self, file_path: str, message: str) -> ICanSaving:
-        return lsb.hide(file_path, message)
+        pass
 
     def reveal(self, carrier_path: str) -> str:
-        return lsb.reveal(carrier_path)
+        pass
